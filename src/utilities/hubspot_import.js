447e5b7dee7784b362deb35b736e4f2e8662d0f0
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-09-03 11:20:58 +0800

    adjusted base url

--- src/utilities/hubspot_import.js
+++ src/utilities/hubspot_import.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import FormData from 'form-data';
 
-const BASE_URL ="http://localhost:8080";
+const BASE_URL ="https://react-tws-hubspot-be-a7eecd7171c3.herokuapp.com";
 
 async function importToHubspot (fileName, contactBlob, companyBlob, toggleModal) {
   const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
