ee8112371ce64fcd81a63c9af587b6671b746761
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-09-13 12:30:46 +0800

    added uploadInvalidContacts function and transfer google api request to backend server

--- src/utilities/hubspot_import.js
+++ src/utilities/hubspot_import.js
@@ -4,6 +4,27 @@ import FormData from 'form-data';
 // const BASE_URL ="https://react-tws-hubspot-be-a7eecd7171c3.herokuapp.com";
 const BASE_URL ="http://localhost:8080";
 
+export async function uploadInvalidContacts(filename, invalidContactBlob){
+  let form = new FormData();
+  let fileName = `Invalid_Contacts_${filename}`;
+  form.append('file', invalidContactBlob, fileName);
+
+  try {
+    const res = await axios.post(`${BASE_URL}/upload-to-drive`, form, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      }
+    });
+
+    if(res.status === 200){
+      console.log(`This is the link for the upload: ${JSON.stringify(res.data,null,1)}`);
+    }else{
+      console.log(`Failed to upload file to gdrive. Status ${res.status}`);
+    }
+  } catch (error) {
+    console.log(`Upload Invalid Contacts to Drive Failed. Error: ${error}`);
+  }
+}
 
 export async function sendToServer(fileName, contactBlob, companyBlob, contactBlob2, projectBlob, toggleModal, setLoading) {
   let form = new FormData();
