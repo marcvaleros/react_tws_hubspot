7443ed6609983f9f65081a3f255cf71353c4e65f
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-10-01 21:32:05 +0800

    added filtermodal and rename tool to Zach-O-Matic

--- src/components/filterModal.js
+++ src/components/filterModal.js
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 import { AdjustmentsHorizontalIcon } from '@heroicons/react/24/outline';
+import FilterCheckboxes from './filterCheckbox';
 
 export default function FilterModal({toggleModal}) {
   const [open, setOpen] = useState(true);
@@ -24,20 +25,24 @@ export default function FilterModal({toggleModal}) {
             className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
           >
             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-              <div className="sm:flex sm:items-center">
-                <div className={`mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-100 sm:mx-0 sm:h-10 sm:w-10`}>
-                  <AdjustmentsHorizontalIcon aria-hidden="true" className={`h-6 w-6 text-orange-600 `} />
-                </div>
+              <div className="flex flex-col space-y-2 sm:flex sm:items-center">
                 
-                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                  <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                    Filter Settings
-                  </DialogTitle>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500"></p>
+                <div className="self-start text-center sm:ml-4 sm:mt-0 sm:text-left gap-4">
+                  
+                  <div className='flex flex-row justify-center items-center gap-4'>
+                    <div className='mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-50 sm:mx-0 sm:h-10 sm:w-10'>
+                      <AdjustmentsHorizontalIcon aria-hidden="true" className='h-6 w-6 text-orange-600' />
+                    </div>
+                    <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                      Filter Settings
+                    </DialogTitle>
                   </div>
+
                 </div>
-                
+                  
+                <div className='w-full'>
+                  <FilterCheckboxes/>
+                </div>
                 
               </div>
             </div>
