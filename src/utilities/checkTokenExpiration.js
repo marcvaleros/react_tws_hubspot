698874cbcdf1e13e78432b132b6c6db8f45635fe
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-10-14 21:50:26 +0800

    refactor: remove logs for token

--- src/utilities/checkTokenExpiration.js
+++ src/utilities/checkTokenExpiration.js
@@ -7,11 +7,5 @@ export default function isTokenExpired() {
   const decodedToken = JSON.parse(atob(token.split('.')[1])); 
   const currentTime = Date.now() / 1000; // get current time in seconds
 
-  if(decodedToken.exp < currentTime){
-    console.log('Token is expired');
-  }else{
-    console.log('Token is not expired');
-  }
-
   return decodedToken.exp < currentTime;
 }
